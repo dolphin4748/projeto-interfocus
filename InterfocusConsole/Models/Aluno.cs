a0aa6cd26d5327325573e0e2d916cbe74863dad7
matheus lucio <151792207+dolphin4748@users.noreply.github.com>
2025-06-29 09:36:12 -0300

    Add files via upload

---
+++ InterfocusConsole/Models/Aluno.cs
@@ -0,0 +1,52 @@
+﻿using InterfocusConsole.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace InterfocusConsole.Models
+{
+    public class Aluno : INomeavel, IEntidade
+    {
+        //private string nome;
+        //public string GetNome()
+        //{
+        //    return nome;
+        //}
+        //public void SetNome(string nome)
+        //{
+        //    this.nome = nome;
+        //}
+        public long Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        [RegularExpression(@"^\d{5}-\d{3}$", 
+            ErrorMessage = "O CEP está com formato inválido")]
+        public string Cep { get; set; }
+        public DateTime? DataNascimento { get; set; }
+
+        public int Idade
+        {
+            get
+            {
+                if (!DataNascimento.HasValue)
+                {
+                    return 0;
+                }
+                var anoAtual = DateTime.Today.Year;
+                var idade = anoAtual - DataNascimento.Value.Year;
+                var aniversario = DateTime.Today.AddYears(-idade);
+                if (DataNascimento > aniversario) idade--;
+                return idade;
+            }
+        }
+
+        public DateTime DataCadastro { get; private set; }
+        = DateTime.Now;
+
+        public virtual string GetPrintMessage()
+        {
+            // getcodigo e getnome
+            return $"{Id} - {Nome}"; // f"" python
+        }   
+    }
+}
---
+++ TreinamentoAPI/Controllers/AlunoController.cs
@@ -0,0 +1,73 @@
+﻿using InterfocusConsole;
+using InterfocusConsole.Models;
+using InterfocusConsole.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TreinamentoAPI.Controllers
+{
+    [Route("api/aluno")]
+    public class AlunoController : ControllerBase
+    {
+        private readonly AlunoService servico;
+
+        public AlunoController(AlunoService servico)
+        {
+            this.servico = servico;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string pesquisa)
+        {
+            return string.IsNullOrEmpty(pesquisa) ? 
+                Ok(servico.Consultar()) :
+                Ok(servico.Consultar(pesquisa));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Aluno aluno)
+        {
+            // AlunoService, AlunoBusiness
+            // controllers: camada de acesso
+            // services: camada de negócio
+            // repositories: camada de dados
+            if (servico.Cadastrar(aluno, out List<MensagemErro> erros))
+            {
+                return Ok(aluno);
+            }
+            return UnprocessableEntity(erros);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Aluno aluno)
+        {
+            var resultado = servico.Editar(aluno, out _);
+            if (resultado == null)
+            {
+                return NotFound();
+            }
+            return Ok(resultado);
+        }
+
+        [HttpDelete("{codigo}")]
+        public IActionResult Delete(long codigo)
+        {
+            var resultado = servico.Deletar(codigo);
+            if (resultado == null)
+            {
+                return NotFound();
+            }
+            return Ok(resultado);
+        }
+
+        [HttpGet("{codigo}")]
+        public IActionResult GetById(long codigo)
+        {
+            var resultado = servico.ConsultarPorCodigo(codigo);
+            if (resultado == null)
+            {
+                return NotFound();
+            }
+            return Ok(resultado);
+        }
+    }
+}
